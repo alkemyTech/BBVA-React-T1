a5104afead8ab0505e07802fb248f6ca23b21c15
Agustin <agus.conde18@gmail.com>
2022-05-09 16:48:39 -0300

    Creacion de funcion Put dentro del servicio 'privateApiService.js'

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const API_BASE_URL = 'https://jsonplaceholder.typicode.com/'
+
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 1                //Aqui va el ID del equipo!!
     }
 }
 
@@ -12,4 +14,19 @@ const Get = () => {
     .catch(err => console.log(err))
 }
 
+
+/**
+ * Actualiza los datos de la ruta destino
+ * @function
+ * @param id Id del recurso a actualizar
+ * @param route Ruta del recurso, se ingresa sin las barras, ej: route = "slides"
+ * @param body Se pasa el objeto del recurso a actualizar
+ * 
+ * @returns Promesa de axios, se debe capturar los metodos then y catch en caso de error
+ */
+const Put = (id, route, body) => {
+    return axios.put(`${API_BASE_URL}${route}/${id}`,body,config)
+}
+
 export default Get
+export { Put }
