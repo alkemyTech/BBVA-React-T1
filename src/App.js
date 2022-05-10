93f68b855677c342dafb3760ccd59d60e373511b
Agustin <agus.conde18@gmail.com>
2022-05-10 12:42:39 -0300

    Se elimino el componente inutilizado Counter, cuya funcion era mostrar una pantalla con un contador con botones (el default de cuando uno crea el proyecto react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
