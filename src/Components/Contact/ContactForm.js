02147f4802ab6a74669ac752fd92cb3817d87aee
NahuelAlfano <nahualfano@gmail.com>
2022-05-10 11:33:11 -0300

    validacion de numero y placeholders

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,7 +1,13 @@
 import { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
 import '../../Components/FormStyles.css';
 
 const ContactForm = () => {
+
+    const numberRegex = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
+
+
+    // const navigate = useNavigate();
 
     const [initialValues, setInitialValues] = useState({
         name: '',
@@ -23,15 +29,23 @@ const ContactForm = () => {
     }
 
     const handleSubmit = (e) => {
-
+        e.preventDefault();
     }
+
+
+
+    // const handleBtn = () => {
+    //     history.go(-1);
+    // }
 
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
-            <input className="input-field" type="email" name="email" value = {initialValues.email || ''} onChange={handleChange}></input>
-            <input className="input-field" type="number" name="phone" value = {initialValues.phone || ''} onChange={handleChange} min = "8"></input>
-            <input className="input-field" type="text" name="message" value = {initialValues.message || ''} onChange={handleChange}></input>
+            <input className="input-field" type="email" name="email" value = {initialValues.email || ''} onChange={handleChange} placeholder="Email"></input>
+            <input className="input-field" type="number" name="phone" value = {initialValues.phone || ''} onChange={handleChange} pattern={numberRegex} placeholder="Telefono de contacto"></input>
+            <textarea className="input-field" type="text" name="message" value = {initialValues.message || ''} onChange={handleChange} placeholder = "Escriba su mensaje"></textarea>
+            <button className="submit-btn" type="submit">Enviar consulta</button>
+            {/* <button className="btn" onclick="handleBtn()">Volver a inicio</button> */}
         </form>
     )
 }
