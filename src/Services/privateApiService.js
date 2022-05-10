0339f7976713662ecd97f8e724eb0f4157cf5a6c
jenkobud <jenkobudne@gmail.com>
2022-05-10 11:39:54 -0300

    feature/FS01-29 | Delete method for private API, basic function without return

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -6,10 +6,29 @@ const config = {
     }
 }
 
+const API_BASE_URL = 'https://ongapi.alkemy.org/api/docs#/';
+
 const Get = () => {
     axios.get('https://jsonplaceholder.typicode.com/users', config)
     .then(res => console.log(res))
     .catch(err => console.log(err))
+}
+
+/* 
+ *  peticiones DELETE a los endpoints privados
+ *    REQUISITOS:
+ *     -route := ruta destino
+ *     -id := identificador de usuario
+ *    EJECUCIÓN: 
+ *     Llama a axios.delete() con la ruta destino e útiliza el id para autentificar con el servidor,
+ *      y se tratan los casos then() y catch()
+ */
+
+export const Delete = (rout, id) => {
+    axios.delete(`${API_BASE_URL}/${rout}/${id}`, config)
+    .then(res => console.log(res))
+    .catch(err => console.err(err));
+    //Debo devolver response...
 }
 
 export default Get
