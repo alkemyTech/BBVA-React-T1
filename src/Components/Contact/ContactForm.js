600b6cf4038727b92157e4fdf63e7f661c16662c
NahuelAlfano <nahualfano@gmail.com>
2022-05-10 12:27:56 -0300

    section, titulos y botones

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import '../../Components/FormStyles.css';
 
 const ContactForm = () => {
 
+    // valida 0 y codigo de país opcional. codigo area obligatorio 
+
     const numberRegex = /^(?:(?:00)?549?)?0?(?:11|[2368]\d)(?:(?=\d{0,2}15)\d{2})??\d{8}$/;
 
-
-    // const navigate = useNavigate();
+    let history = useHistory();
 
     const [initialValues, setInitialValues] = useState({
         name: '',
@@ -30,23 +31,43 @@ const ContactForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const phone = e.target.phone.value;
+        
+        if(name !== '' && email !== ''){
+            if(numberRegex.test(phone)){
+                console.log('Se envio correctamente')
+            }else{
+                console.log('Ingrese un numero valido')
+            }
+
+        } else{
+            console.log('Los datos ingresados son invalidos')
+        }
     }
-
-
 
     const handleBtn = () => {
         console.log('holaa');
+        history.push('/')
     }
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
-            <input className="input-field" type="email" name="email" value = {initialValues.email || ''} onChange={handleChange} placeholder="Email"></input>
-            <input className="input-field" type="number" name="phone" value = {initialValues.phone || ''} onChange={handleChange} pattern={numberRegex} placeholder="Telefono de contacto"></input>
-            <textarea className="input-field" type="text" name="message" value = {initialValues.message || ''} onChange={handleChange} placeholder = "Escriba su mensaje"></textarea>
-            <button className="submit-btn" type="submit">Enviar consulta</button>
-            <button className="btn" onClick={handleBtn}>Volver a inicio</button>
-        </form>
+        <>
+            <section>
+                <h2>¿Queres contribuir?</h2>
+                <button>Contribuir</button>
+                <h2>¡Contactate con nosotros!</h2>
+                <form className="form-container" onSubmit={handleSubmit}>
+                    <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name" required></input>
+                    <input className="input-field" type="email" name="email" value = {initialValues.email || ''} onChange={handleChange} placeholder="Email" required></input>
+                    <input className="input-field" type="number" name="phone" value = {initialValues.phone || ''} onChange={handleChange} pattern={numberRegex} placeholder="Telefono de contacto" required></input>
+                    <textarea className="input-field" type="text" name="message" value = {initialValues.message || ''} onChange={handleChange} placeholder = "Escriba su mensaje" required></textarea>
+                    <button className="submit-btn" type="submit">Enviar consulta</button>
+                </form>
+                <button className="submit-btn" onClick={handleBtn} required>Volver a inicio</button>
+            </section>
+        </>
     )
 }
 
