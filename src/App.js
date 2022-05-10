316037c47d7deb06ecf9499d1c2311ee6b8ac6e4
Agustin <agus.conde18@gmail.com>
2022-05-10 12:11:42 -0300

    Se configura el ruteo de '/Nosotros'

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import Nosotros from './Components/About/Nosotros'
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
+          <Route path="/Nosotros" component={Nosotros} />
         </Switch>
       </BrowserRouter>
     <div className="App">
