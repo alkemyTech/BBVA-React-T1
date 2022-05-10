cc47238abaf8e06b7cfcc283df1e01097271d2a8
Agustin <agus.conde18@gmail.com>
2022-05-10 12:11:14 -0300

    Se crea el componente Nosotros

---
+++ src/Components/About/Nosotros.js
@@ -0,0 +1,21 @@
+
+/**
+ * En esta seccion dispondremos el componente Nosotros, que se encontrara
+ * bajo la ruta /Nosotros, el cual podremos ver informacion acerca de la ONG
+ * Dicha informacion se renderizará de forma dinamica desde una API
+ * @returns jsx para mostrar en pantalla
+ */
+
+const Nosotros = () => {
+
+   
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+
+export default Nosotros
