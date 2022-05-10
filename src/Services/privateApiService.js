7f2f7eb4846fad6c26292dd13d6fc428b6ed27a4
Matías Vera <matias_vera@outlook.com>
2022-05-10 11:10:09 -0300

    Reemplacé la función Get que ahora retorna res.data, agregué la variable access_token como placeholder

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,26 +1,20 @@
 import axios from 'axios';
 
 const baseUrl = 'https://ongapi.alkemy.org/api/'
+const access_token = ""
 
 const config = {
     headers: {
-        Group: 01,                //Aqui va el ID del equipo!!
+        Group: 1,                //Aqui va el ID del equipo!!
         Authorization: `token ${access_token}`
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
+const Get = (endpoint, id = "") => {
+    axios.get(`${baseUrl}${endpoint}/${id}` ,config)
+    .then(res => {return res.data})
     .catch(err => console.log(err))
 }
 
-/*
-const Get = (endpoint, id = "") => {
-    axios.get(`${baseUrl}${endpoint}/${id}`, config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
-*/
 
 export default Get
