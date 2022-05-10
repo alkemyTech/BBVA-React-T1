2764177d5168e7ba0ba9453b30792c14d563ac76
Matías Vera <matias_vera@outlook.com>
2022-05-10 14:16:12 -0300

    Cambie nombre de la constante

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
 
-const baseUrl = 'https://ongapi.alkemy.org/api/'
+const API_BASE_URL = 'https://ongapi.alkemy.org/api/'
 const access_token = ""
 
 const config = {
     headers: {
-        Group: 01,                //Aqui va el ID del equipo!!
+        Group: 1,                //Aqui va el ID del equipo!!
         Authorization: `token ${access_token}`
     }
 }
 
-const Get = (endpoint, id = "") => {
-    axios.get(`${baseUrl}${endpoint}/${id}` ,config)
+ export const Get = (endpoint, id = "") => {
+    axios.get(`${API_BASE_URL}${endpoint}/${id}`, config)
     .then(res => res.data)
     .catch(err => err)
 }
 
-
-export default Get
