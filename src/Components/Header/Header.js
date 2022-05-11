b35e6f1b89cb5a9f4c9b331908bdc1754b22a7ef
Matías Vera <matias_vera@outlook.com>
2022-05-11 10:21:29 -0300

    Validación por existencia de token en localStorage

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,43 +3,50 @@ import { BrowserRouter as Router, NavLink } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-    const navMenu = [
-        { name: "Inicio", link: "/", requiresLogIn: false },
-        { name: "Nosotros", link: "/nosotros", requiresLogIn: false },
-        { name: "Contacto", link: "/contacto", requiresLogIn: false },
-        { name: "School", link: "/school", requiresLogIn: false },
-        { name: "Toys", link: "/toys", requiresLogIn: false }
-      ];    
-    
-    return (
-        <div className="header">
-          <Router>
-            <div className="header_contents">
-              <img
-                className="header_logo"
-                src="/images/assets/logo.png"
-                alt="logo"
-              />
-              <div className="nav_links">
-                <ul className="header_list">
-                  {navMenu.map(
-                    (item) =>
-                        <li className={`nav_item`}>
-                          <NavLink
-                            className={(navData) =>
-                              navData.isActive ? "active" : "link"
-                            }
-                            to={item.link}
-                          >
-                            {item.name}
-                          </NavLink>
-                        </li>
-                      
-                  )}
-                </ul>
-              </div>
-            </div>
-          </Router>
+  const navMenu = [
+    { name: "Inicio", link: "/", requiresLogIn: false },
+    { name: "Nosotros", link: "/nosotros", requiresLogIn: false },
+    { name: "Contacto", link: "/contacto", requiresLogIn: false },
+    { name: "School", link: "/school", requiresLogIn: false },
+    { name: "Toys", link: "/toys", requiresLogIn: false }
+  ];
+
+  //Validate if the user has token in localStorage
+  let isLoggedIn = false;
+  !localStorage.getItem("token") === null ||
+  !localStorage.getItem("token") === undefined
+    ? (isLoggedIn = true)
+    : (isLoggedIn = false);
+
+  return (
+    <div className="header">
+      <Router>
+        <div className="header_contents">
+          <img
+            className="header_logo"
+            src="/images/assets/logo.png"
+            alt="logo"
+          />
+          <div className="nav_links">
+            <ul className="header_list">
+              {navMenu.map((item) => (isLoggedIn || (!isLoggedIn && !item.requiresLogIn)) && (
+                <li className={`nav_item`}>
+                  <NavLink
+                    className={(navData) =>
+                      navData.isActive ? "active" : "link"
+                    }
+                    to={item.link}
+                  >
+                    {item.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      );
-    }
+      </Router>
+    </div>
+  );
+}
+
+export default Header;
