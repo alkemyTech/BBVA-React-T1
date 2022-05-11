2fab04f45e0667538fd5e621094e113d349c0b74
Agustin <agus.conde18@gmail.com>
2022-05-11 14:44:48 -0300

    Se eliminaron archivos no necesarios que venian por default en la creacion del proyecto

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import Nosotros from './Components/About/Nosotros'
+import Header from './Components/Header/Header';
 
 function App() {
   return (
