79ee44a181d9b2020b1465f0e0fa30d3aeb99794
Agustin <agus.conde18@gmail.com>
2022-05-12 17:48:58 -0300

    Incorporacion de spinner

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -5,6 +5,9 @@ import { useParams } from 'react-router-dom';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import './ActivitiesForm.css'
+import { useEffect } from 'react';
+import { Get } from '../../Services/publicApiService';
+import Spinner from './../Spinner/Spinner';
 
 const ActivitiesForm = () => {
     const [initialValues, setInitialValues] = useState({
@@ -17,6 +20,10 @@ const ActivitiesForm = () => {
     const { idActividad } = useParams();
 
   
+    useEffect(() => {
+        
+        
+    }, );
 
     const handleChange = (e) => {
         if(e.target.name === 'name'){
@@ -40,28 +47,31 @@ const ActivitiesForm = () => {
 
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            
-            <TextField id="outlined-basic" label="Titulo de la actividad" variant="outlined"  
-                        type="text" name="name" value={initialValues.name} onChange={handleChange}/>
-            
-         <CKEditor
-                    editor={ ClassicEditor }
-                    data={ckEditorData}
-                    onChange={ ( event, editor ) => setCkData(editor.getData()) }
-                /> 
+        <div className="globalContainer">
+            <Spinner visible={false} className="spinner"  /> 
+            <form className="form-container" onSubmit={handleSubmit}>
+                
+                <TextField id="outlined-basic" label="Titulo de la actividad" variant="outlined"  
+                            type="text" name="name" value={initialValues.name} onChange={handleChange}/>
+                
+            <CKEditor
+                        editor={ ClassicEditor }
+                        data={ckEditorData}
+                        onChange={ ( event, editor ) => setCkData(editor.getData()) }
+                    /> 
 
-            <TextField id="outlined-basic" label="Image src URL" variant="outlined"  
-                        type="text" name="srcUrlImage" value={initialValues.srcUrlImage} onChange={handleChange}/>
+                <TextField id="outlined-basic" label="Image src URL" variant="outlined"  
+                            type="text" name="srcUrlImage" value={initialValues.srcUrlImage} onChange={handleChange}/>
 
 
-            {initialValues.srcUrlImage.length>5 &&
-                <img  src={initialValues.srcUrlImage} alt="La imagen no pudo ser cargada, verifique la URL"
-                className='imgPrueba'/>
-            
-            }
-            <button className="submit-btn" type="submit" onClick={submitData()}>Send</button>
-        </form>
+                {initialValues.srcUrlImage.length>5 &&
+                    <img  src={initialValues.srcUrlImage} alt="La imagen no pudo ser cargada, verifique la URL"
+                    className='imgPrueba'/>
+                
+                }
+                <button className="submit-btn" type="submit" onClick={submitData()}>Send</button>
+            </form>
+        </div>
     );
 }
  
