97c6a929892b2ad2d8fc4b829b036fa0bd335ba4
Agustin <agus.conde18@gmail.com>
2022-05-12 15:37:43 -0300

    Incorporacion CKAN, trabajando en estilos

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import '../FormStyles.css';
 import { TextField } from '@mui/material';
 import { useParams } from 'react-router-dom';
-
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import './ActivitiesForm.css'
 
 const ActivitiesForm = () => {
     const [initialValues, setInitialValues] = useState({
         name: '',
+        srcUrlImage: '',
         description: ''
     });
+
+    const [ckEditorData, setCkData] = useState("")
     const { idActividad } = useParams();
-    
+
   
 
     const handleChange = (e) => {
@@ -19,6 +24,9 @@ const ActivitiesForm = () => {
         } if(e.target.name === 'description'){
             setInitialValues({...initialValues, description: e.target.value})
         }
+        if(e.target.name === 'srcUrlImage'){
+            setInitialValues({...initialValues, srcUrlImage: e.target.value})
+        }
     }
 
     const handleSubmit = (e) => {
@@ -26,17 +34,33 @@ const ActivitiesForm = () => {
         console.log(initialValues);
     }
     
+    const submitData = () =>{
+
+    }
+
+
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             
             <TextField id="outlined-basic" label="Titulo de la actividad" variant="outlined"  
                         type="text" name="name" value={initialValues.name} onChange={handleChange}/>
-            <TextField id="outlined-basic" label="Titulo de la actividad" variant="outlined"  
-                        type="text" name="name" value={initialValues.name} onChange={handleChange}/>
-             <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
-          
             
-            <button className="submit-btn" type="submit">Send</button>
+         <CKEditor
+                    editor={ ClassicEditor }
+                    data={ckEditorData}
+                    onChange={ ( event, editor ) => setCkData(editor.getData()) }
+                /> 
+
+            <TextField id="outlined-basic" label="Image src URL" variant="outlined"  
+                        type="text" name="srcUrlImage" value={initialValues.srcUrlImage} onChange={handleChange}/>
+
+
+            {initialValues.srcUrlImage.length>5 &&
+                <img  src={initialValues.srcUrlImage} alt="La imagen no pudo ser cargada, verifique la URL"
+                className='imgPrueba'/>
+            
+            }
+            <button className="submit-btn" type="submit" onClick={submitData()}>Send</button>
         </form>
     );
 }
