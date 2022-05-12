ac9d9c2320a8a1782289e7f76094b14b135151f0
Agustin <agus.conde18@gmail.com>
2022-05-12 20:03:48 -0300

    Soluciones de code review

--- src/Components/About/Nosotros.js
+++ src/Components/About/Nosotros.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import '../../App.css'
 import './Nosotros.css'
 import { Get } from './../../Services/privateApiService';
 import Spinner from '../Spinner/Spinner'
