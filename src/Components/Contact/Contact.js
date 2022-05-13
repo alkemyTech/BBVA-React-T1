076d17481183646615fab8d4a56ec364c0fb0b53
Erika Farias <erika.aldana.farias@gmail.com>
2022-05-13 13:08:04 -0300

    First commit

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ContactForm from "./ContactForm";
+
+const Contact = ({}) => {
+  return (
+    <>
+      <div className="">
+        <Contact />
+        <div className="contact-data">
+            
+        </div>
+      </div>
+    </>
+  );
+};
