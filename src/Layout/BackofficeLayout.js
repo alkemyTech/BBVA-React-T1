6545110a44a3fcef4cfd046e1ab9639e269c4fc7
jenkobud <jenkobudne@gmail.com>
2022-05-13 10:51:32 -0300

    feature/FS01-106 | Layout de backoffice -> estructura básica

---
+++ src/Layout/BackofficeLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export const BackofficeLayout = ({chilren}) => {
+  return(
+    <>
+      //HeaderBackoffice
+      {children}
+      //FooterBackoffice
+    </>
+  );
+};
