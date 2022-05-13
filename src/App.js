b856ac975e8cdc735856988c53972876831fc607
Agustin <agus.conde18@gmail.com>
2022-05-13 14:42:33 -0300

    Incorporacion de componentes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import PublicLayout from "./Layout/PublicLayout";
 import RegisterForm from "./Components/Auth/RegisterForm";
 import LoginForm from "./Components/Auth/LoginForm";
 
-
 function App() {
   return (
     <>
@@ -25,10 +24,11 @@ function App() {
         <BrowserRouter>
           <Switch>
             {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+
+            <Route path="/backoffice/slides/:id" component={SlidesForm} />
             <Route path="/create-activity" component={ActivitiesForm} />
             <Route path="/create-category" component={CategoriesForm} />
             <Route path="/create-news" component={NewsForm} />
-            <Route path="/backoffice/create-slide" component={SlidesForm} />
             <Route path="/create-testimonials" component={TestimonialForm} />
             <Route path="/create-user" component={UserForm} />
             <Route path="/create-member" component={MembersForm} />
--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,31 +1,64 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import '../FormStyles.css';
+import { Snackbar , Alert,TextField } from '@mui/material';
+import Spinner from './../Spinner/Spinner';
+import { useHistory } from "react-router-dom";
 
 const SlidesForm = () => {
+
+    const history = useHistory();
+    
     const [initialValues, setInitialValues] = useState({
         name: '',
         description: ''
     });
 
+    const { id } = useParams();
+    const [loaded , setLoaded] = useState(false)
+
+    const [snack, setSnack] = useState({
+        open: false,
+        message: "",
+        severity: "error",
+    })
+
+    const creacionForm = id === "create";
+
+
     const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
+         setInitialValues({...initialValues, [e.target.name]: e.target.value})
     }
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         console.log(initialValues);
     }
+    const onCloseSnack = () =>{
+        setSnack({...snack, open:false})
+    }
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <div className="globalContainer">
+            <Spinner visible={!loaded} className="spinner"  />  
+
+            <form className="form-container" onSubmit={handleSubmit}>
+                {id}
+                <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
+                <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
+                <button className="submit-btn" type="submit">Send</button>
+            </form>
+
+            <Snackbar
+            open={snack.open}
+            severity={snack.severity}
+            autoHideDuration={3000}
+            onClose={onCloseSnack}>
+            <Alert onClose={onCloseSnack} severity={snack.severity} sx={{ width: '100%' }}>
+                {snack.message}
+            </Alert>
+        </Snackbar>
+     </div>
     );
 }
  
