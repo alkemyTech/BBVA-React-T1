b71d7463b70b041bd4c0db1fffa1cd06e1fb5f8f
Erika Farias <erika.aldana.farias@gmail.com>
2022-05-13 16:32:08 -0300

    Contact layout, not finished

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,15 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import ContactForm from "./ContactForm";
+import { Get } from "../../Services/privateApiService";
 
-const Contact = ({}) => {
+const Contact = () => {
+  //   [contact, setContact] = useState({});
+
+  const getContactData = async () => {
+    const res = await Get("/organization");
+    return res.data.data;
+  };
+
+  getContactData();
   return (
     <>
-      <div className="">
-        <Contact />
+      <h1 className="contact-title">Contacto</h1>
+      <div className="container-body-contact">
+        <div className="contact-form">
+          <ContactForm />
+        </div>
         <div className="contact-data">
-            
+          <h2 className="subtitle-contact">
+            Medios de e información de contacto
+          </h2>
+          <ul className="list-contact"></ul>
         </div>
       </div>
     </>
   );
 };
+
+export default Contact;
