3ca1ed4604b1ab4f060900ef05d20e9de367ea2a
Agustin <agus.conde18@gmail.com>
2022-05-14 10:55:11 -0300

    Solucion concatenado url

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -10,7 +10,7 @@ const config = {
 
 const Get = (route, id = null) => {
   axios
-    .get(`${process.env.REACT_APP_URL_BASE_ENDPOINT+route+'/'+id}`, config)
+    .get(`${route+'/'+id}`, config)
     .then((res) => {
       return res;
     })
@@ -30,7 +30,7 @@ const Get = (route, id = null) => {
 */
 export const Post = async (route, bodyObj) => {
     try {
-        const res = await axios.post(`${process.env.REACT_APP_URL_BASE_ENDPOINT+route}`, bodyObj, config);
+        const res = await axios.post(`${route}`, bodyObj, config);
         return res;
     } catch (err) { return err; }
 }
