cc61c4990f0218b3c2b8009eb798652712e30b8c
Agustin <agus.conde18@gmail.com>
2022-05-16 11:00:52 -0300

    Cambio slider para que interprete TAGS HTML

--- src/Components/Slides/Show/ShowSlides.js
+++ src/Components/Slides/Show/ShowSlides.js
@@ -1,9 +1,13 @@
+import React from "react";
+import SliderHome from "../../SliderHome/SliderHome";
 
 const ShowSlides = () => {
 
+    const slidesEndpoint=process.env.REACT_APP_URL_SLIDES;
+    
     return(
         <>
-        d
+                <SliderHome endpoint={slidesEndpoint}/>
         </>
     )
 }
