f72fba3df203c4f6d10d09a59efa669329a99079
Agustin Adrian Conde <agus.conde18@gmail.com>
2022-05-17 14:04:14 -0300

    Creacion de transicion

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ import LoginForm from "./Components/Auth/LoginForm";
 import { News } from "./Components/News/News";
 import UsersList from "./Components/Users/UsersList";
 import Contact from "./Components/Contact/Contact";
+import { AnimatedSwitch } from 'react-router-transition';
 
 function App() {
   return (
@@ -29,41 +30,50 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Switch>
-            {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
+            <AnimatedSwitch
+              atEnter={{ opacity: 0 }}
+              atLeave={{ opacity: 0 }}
+              atActive={{ opacity: 1 }}
+              className="switch-wrapper"
+            >
+            
+              {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
 
-            <Route path="/backoffice/slides/create" component={SlidesForm} />
-            <Route path="/backoffice/slides/:id" component={SlidesForm} />
-            <Route path="/create-activity" component={ActivitiesForm} />
+              <Route path="/backoffice/slides/create" component={SlidesForm} />
+              <Route path="/backoffice/slides/:id" component={SlidesForm} />
+              <Route path="/create-activity" component={ActivitiesForm} />
 
-            <Route path="/backoffice/activity/:id" component={ActivitiesForm} />
-            <Route path="/backoffice/activity" component={ActivitiesForm} />
-            <Route path="/activities" component={ActivitiesList} />
+              <Route path="/backoffice/activity/create" component={ActivitiesForm} />
+              <Route path="/backoffice/activity/:id" component={ActivitiesForm} />
+              
+              <Route path="/activities" component={ActivitiesList} />
 
-            <Route
-              path="/backoffice/create-category"
-              component={CategoriesForm}
-            />
-            <Route path="/backoffice/create-news" component={NewsForm} />
-            <Route
-              path="/backoffice/create-testimonials"
-              component={TestimonialForm}
-            />
-            <Route path="/backoffice/users/create" component={UserForm} />
-            <Route path="/backoffice/create-member" component={MembersForm} />
-            <Route path="/backoffice/create-project" component={ProjectsForm} />
-            <Route path="/backoffice/users" component={UsersList} />
+              <Route
+                path="/backoffice/create-category"
+                component={CategoriesForm}
+              />
+              <Route path="/backoffice/create-news" component={NewsForm} />
+              <Route
+                path="/backoffice/create-testimonials"
+                component={TestimonialForm}
+              />
+              <Route path="/backoffice/users/create" component={UserForm} />
+              <Route path="/backoffice/create-member" component={MembersForm} />
+              <Route path="/backoffice/create-project" component={ProjectsForm} />
+              <Route path="/backoffice/users" component={UsersList} />
 
-            <PublicLayout>
-              <Route path="/register" component={RegisterForm} />
-              <Route path="/login" component={LoginForm} />
-              <Route path="/news" component={News} />
-              <Route path="/nosotros" component={Nosotros} />
-              <Route path="/news" />
-              <Route path="/testimonials" />
-              <Route path="/contact" component={Contact} />
-              <Route path="/school-campaign" component={SchoolCampaign} />
-              <Route path="/toys-campaign" component={ToysCampaign} />
-            </PublicLayout>
+              <PublicLayout>
+                <Route path="/register" component={RegisterForm} />
+                <Route path="/login" component={LoginForm} />
+                <Route path="/news" component={News} />
+                <Route path="/nosotros" component={Nosotros} />
+                <Route path="/news" />
+                <Route path="/testimonials" />
+                <Route path="/contact" component={Contact} />
+                <Route path="/school-campaign" component={SchoolCampaign} />
+                <Route path="/toys-campaign" component={ToysCampaign} />
+              </PublicLayout>
+            </AnimatedSwitch>
           </Switch>
         </BrowserRouter>
       </div>
