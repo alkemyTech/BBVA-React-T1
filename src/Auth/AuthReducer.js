3b40580298c145ebb401a270a89df8534af869c6
Agustin <agus.conde18@gmail.com>
2022-05-17 00:17:38 -0300

    Trabajando en auth

---
+++ src/Auth/AuthReducer.js
@@ -0,0 +1,19 @@
+const AUTHENTICATED = "loggedIn";
+const LOGOUT = "loggedOut";
+
+export const stateUser= () => {
+    username: "",
+    token: "",
+    rol: []
+}
+
+export const LogIn = ({email,name,token}) => {
+
+
+}
+
+export const RegisterUser = ({})
+
+export const LogOut = () =>{
+
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+
+//let createStoreWithMiddleware = applyMiddleware(thunkMiddleware, api)(createStore)
+
+let store = createStoreWithMiddleware(quotesApp)
+
+let root  = ReactDOM.createRoot(document.getElementById('root'))
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
