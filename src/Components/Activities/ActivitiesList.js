9e38547b6b87a498cf468f3007fc0d0fb97b865b
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-16 23:54:29 -0300

    cambio estructura

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,32 +1,75 @@
-import React from 'react';
-import '../CardListStyles.css';
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableCell,
+  TableBody,
+  TableRow,
+} from "@material-ui/core";
+import { Edit, Delete } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Get } from "./../../Services/privateApiService";
 
-const ActivitiesList = () => {
-    const activitiesMock = [
-        {id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba'}
-    ];
+function ActivitiesList() {
+  const [data, setData] = useState([]);
 
-    return (
-        <div>
-            <h1>Listado Actividades</h1>
-            <ul className="list-container">
-                {activitiesMock.length > 0 ?
-                    activitiesMock.map((activity) => {
-                        return(
-                            <li className="card-info" key={activity.id}>
-                                <h3>{activity.name}</h3>
-                                <p>{activity.description}</p>
-                            </li>
-                        )
-                    })
-                :
-                    <p>No hay actividades</p>
-                }
-            </ul>
-        </div>
-    );
+  const getUsers = async () => {
+    const response = await Get(process.env.REACT_APP_URL_BASE_ENDPOINT + process.env.REACT_APP_URL_ACTIVITIES_PATH);
+    const usersList = await response.data.data;
+    setData(usersList);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <div className="Activities">
+      <div className="buttons">
+        <Button variant="contained" color="success">
+          <Link className="top-links" to="/backoffice/activity/create">
+            {" "}
+            New Activities{" "}
+          </Link>
+        </Button>
+      </div>
+      <div className="table">
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Nombre</TableCell>
+                <TableCell>Descripción</TableCell>
+                <TableCell>Imagen</TableCell>
+                <TableCell>Acciones</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data.map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell>{user.name}</TableCell>
+                  <TableCell>{user.description}</TableCell>
+                  <TableCell><img src={user.image} alt="imagen"></img></TableCell>
+                  <TableCell>
+                    <Link
+                      className="edit-icon"
+                      to={`/backoffice/activity/create/${user.id}`}
+                    >
+                      <Edit />
+                    </Link>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <Delete className="delete-icon" />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
 }
- 
+
 export default ActivitiesList;
