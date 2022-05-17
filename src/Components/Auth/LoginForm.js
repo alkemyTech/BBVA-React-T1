747d27fa81521a3c5571c68b2df407036d2caf82
Matías Vera <matias_vera@outlook.com>
2022-05-16 23:02:53 -0300

    Entrar a /login con token en localstorage te redirecciona a la home

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,33 +1,59 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, { useState, useEffect } from "react";
+import "../FormStyles.css";
+import { useHistory } from "react-router-dom";
 
 const LoginForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        email: '',
-        password: ''
-    });
+  const [initialValues, setInitialValues] = useState({
+    email: "",
+    password: "",
+  });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        } if(e.target.name === 'password'){
-            setInitialValues({...initialValues, password: e.target.value})
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
+  const history = useHistory();
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-            <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-            <button className="submit-btn" type="submit">Log In</button>
-        </form>
-    );
-}
- 
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      history.push("/");
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      setInitialValues({ ...initialValues, email: e.target.value });
+    }
+    if (e.target.name === "password") {
+      setInitialValues({ ...initialValues, password: e.target.value });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(initialValues);
+    localStorage.setItem("token", "tokenValueExample");
+  };
+
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <input
+        className="input-field"
+        type="text"
+        name="email"
+        value={initialValues.name}
+        onChange={handleChange}
+        placeholder="Enter email"
+      ></input>
+      <input
+        className="input-field"
+        type="text"
+        name="password"
+        value={initialValues.password}
+        onChange={handleChange}
+        placeholder="Enter password"
+      ></input>
+      <button className="submit-btn" type="submit">
+        Log In
+      </button>
+    </form>
+  );
+};
+
 export default LoginForm;
