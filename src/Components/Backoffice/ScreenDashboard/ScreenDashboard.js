d74a29632d667e242c87c436f9555ea4a13cd768
Matías Vera <matias_vera@outlook.com>
2022-05-18 05:31:57 -0300

    Cree el componente con el listado incial de links

---
+++ src/Components/Backoffice/ScreenDashboard/ScreenDashboard.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ScreenDashboard = () => {
+  const navMenu = [
+    { name: "Novedades", link: "/backoffice/news", icono: "" },
+    { name: "Actividades", link: "/backoffice/activities", icono: "" },
+    { name: "Categorias", link: "/backoffice/categories", icono: "" },
+    { name: "Testimonios", link: "/backoffice/testimonials", icono: "" },
+    { name: "Organización", link: "/backoffice/organization", icono: "" },
+    { name: "Slides", link: "/backoffice/slides", icono: "" },
+    { name: "Usuarios", link: "/backoffice/users", icono: "" },
+    { name: "Miembros", link: "/backoffice/members", icono: "" },
+  ];
+  return <div>ScreenDashboard</div>;
+};
+
+export default ScreenDashboard;
