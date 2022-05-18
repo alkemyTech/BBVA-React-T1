f00cd6418c58009dd889c08abe78631a1a79f6d5
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-18 20:31:32 -0300

    agrego redireccion a home

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, TextField, Button } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import "../FormStyles.css";
 import { Post } from "../../Services/publicApiService";
 
@@ -18,6 +18,7 @@ const RegisterForm = () => {
   });
 
   const token = localStorage.getItem('token');
+  const history = useHistory();
 
   const passErrorMsg =
     "Debe contener un mínimo de 6 caracteres, una letra, un número y un símbolo";
@@ -62,6 +63,8 @@ const RegisterForm = () => {
   }, [password, confirmPassword]);
 
   return (
+    <>
+    {token ? (history.push('/')) : ''}
     <div className="main-container-form">
       <Container maxWidth="sm" className="form-container">
         <h3>Bienvenido/a!</h3>
@@ -127,6 +130,7 @@ const RegisterForm = () => {
         <img className="img-form" alt="" src="/images/login.png" />
       </div>
     </div>
+    </>
   );
 };
 
