1579ebfb6ed8c21aede39f08bcfb89c042119cb7
Martina Pereyra <1martinapereyra@gmail.com>
2022-05-19 18:13:34 -0300

    cambie useForm en rama incorrecta, vuelvo a setear como estaba

--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from "react-router-dom";
+import { useParams, useLocation, useHistory } from "react-router-dom";
 import '../FormStyles.css';
-import {Get, PrivatePost, Put} from "../../Services/privateApiService"
-import { Snackbar , Alert, TextField, MenuItem } from '@mui/material';
-
+import {Get, PrivatePost, Put, Delete} from "../../Services/privateApiService"
+import { Snackbar , Alert } from '@mui/material';
 
 const UserForm = () => {
   const { id } = useParams();  
+  const location = useLocation().pathname.toLocaleLowerCase();
   const history = useHistory();
-
   const [snack, setSnack] = useState({
     open: false,
     message: "",
@@ -23,39 +22,88 @@ const UserForm = () => {
     password:  ''
 })
 
-const showSnack = (text, type) =>{
-    setSnack({...snack, 
-        message: text,
-        open: true,
-        severity: type,
-    })
-}
+
+    //Mensajes de Error:
+    const snackErrorCargaDatos = () =>{
+        setSnack({...snack, 
+        message:"Error en la carga de datos, intente nuevamente mas tarde.",
+        open:true,
+        severity:"error"
+        })
+    }
+
+    const snackErrorRole = () =>{
+        setSnack({...snack, 
+            message:"Debe seleccionar un rol",
+            open:true,
+            severity:"error"
+        }) 
+    }
+
+    const snackErrorName = () =>{
+        setSnack({...snack, 
+            message:"El nombre debe contener al menos 4 letras",
+            open:true,
+            severity:"error"
+        }) 
+    }
+
+    const snackErrorMail = () =>{
+        setSnack({...snack, 
+            message:"El formato de mail es incorrecto",
+            open:true,
+            severity:"error"
+        }) 
+    }
+
+    const snackErrorPassword = () =>{
+        setSnack({...snack, 
+            message:"El password debe contener al menor 8 letras",
+            open:true,
+            severity:"error"
+        }) 
+    }
+
+    const snackErrorImage = () =>{
+        setSnack({...snack, 
+            message:"El formato de la imagen debe ser .jpg o .png",
+            open:true,
+            severity:"error"
+        }) 
+    }
 
 //Obtener datos de usuarios y cargarlos en caso de encontrarlos con el id
   const getUsers = async () => {
-    if(id){
-        await Get(process.env.REACT_APP_URL_BASE_ENDPOINT + "/users/" + id)
-        .then(res => {
-            const data=res.data.data;
-            setInitialValues({
-                ...initialValues,
-                name:data.name || "",
-                email:data.email || "",
-                roleId:data.role_id || "",
-                profileImg: data.profile_image || "",
-                password: data.password || ""
-            })
+    await Get("/users", id)
+    .then(res => {
+        const data=res.data.data;
+        setInitialValues({
+            ...initialValues,
+            name:data.name || "",
+            email:data.email || "",
+            roleId:data.role_id || "",
+            profileImg: data.profile_image || "",
+            password: data.password || ""
         })
-        .catch(() => {
-            showSnack("Error en la carga de datos, intente nuevamente mas tarde.", "error")
-        })
-    }
-    };
+    })
+    .catch(() => {
+         snackErrorCargaDatos()
+    })
+  };
 
     useEffect(() => {
         getUsers()
     }, []);   
 
+
+// Objeto creado a partir de los valores ingresados del form para enviar peticiones
+const userCreated={
+    name: initialValues.name ,
+    email: initialValues.email,
+    role_id: parseInt(initialValues.roleId),
+    password: initialValues.password,
+    profile_image: initialValues.profileImg,
+} 
   
 
 //Validaciones del form
@@ -64,48 +112,56 @@ const formValidation = () =>{
     const imgRegex = new RegExp(/(.jpg|.jpeg|.png)/i)
     let formCorrecto = false;
     if(initialValues.name.length < 4){
-        showSnack("El nombre debe contener al menos 4 letras","error")
+        snackErrorName()
     }else if(Number.isNaN(parseInt(initialValues.roleId)) ){
-        showSnack("Debe seleccionar un rol", "error")
-    }else if(!emailRegexp.test(initialValues.email)){
-        showSnack("El formato de mail es incorrecto", "error")
+        snackErrorRole()
+    }
+    else if(!emailRegexp.test(initialValues.email)){
+        snackErrorMail()
     }else if(initialValues.password.length < 8){
-        showSnack("La contraseña debe contener al menos 8 letras", "error")
+        snackErrorPassword()
     }else if(!imgRegex.test(initialValues.profileImg)){
-        showSnack("Debe subir una imagen en formato jpg o png", "error")
+        snackErrorImage()
     }
     else{
         formCorrecto = true;
-        showSnack("Formulario procesado", "success")
     }
     return formCorrecto
 }
 
+
 //Envio del form y peticiones
 const handleSubmit = async (e)  => {
-
-    const userCreated={
-    name: initialValues.name ,
-    email: initialValues.email,
-    role_id: parseInt(initialValues.roleId),
-    password: initialValues.password,
-    profile_image: initialValues.profileImg,
-    } 
-
     e.preventDefault();
     if(formValidation()){
-        if(id){
-            await Put(process.env.REACT_APP_URL_BASE_ENDPOINT + "/users/" + id, userCreated)
+        if(location.includes("create")){
+           await PrivatePost("/users", userCreated)
             history.push("/backoffice/users") 
-        }else{
-            await PrivatePost(process.env.REACT_APP_URL_BASE_ENDPOINT + "/users", userCreated)
+          }
+        else if(location.includes("edit")){
+            await Put(id,"/users", userCreated)
             history.push("/backoffice/users") 
-        }
+        }else if(location.includes("delete")){
+            await Delete("/users", id)
+            history.push("/backoffice/users") 
+        } 
     }
 }
+
     //Actualiza los datos con los que obtiene de los inputs del form
     const handleChange = (e) => {
-        setInitialValues({...initialValues, [e.target.name]: e.target.value})
+        if(e.target.name === 'name'){
+        setInitialValues({...initialValues, name: e.target.value})
+        }if(e.target.name === 'email'){
+        setInitialValues({...initialValues, email: e.target.value})
+        }if(e.target.name === 'profile-img'){
+        setInitialValues({...initialValues, profileImg: e.target.value})
+        }if(e.target.name === 'password'){
+        setInitialValues({...initialValues, password: e.target.value})
+        }
+        if(e.target.name === 'profile-img'){
+        setInitialValues({...initialValues, profileImg: e.target.value})
+        }
     }
 
     //Formateo de imagen a code64 para enviar a api
@@ -124,36 +180,34 @@ const handleSubmit = async (e)  => {
     
     return (
         <>
-
-        <h1 className="title-back" >{ !id ? "Crear usuario" : "Editar Usuario" }</h1>
-        <form className="form-back"  onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="Nombre y Apellido" variant="outlined"  
-        type="text"  name="name"  
-        value={ initialValues.name } onChange={handleChange}/>
-        <TextField id="outlined-basic" label="Dirección Mail" variant="outlined"  
-        type="text"  name="email" value={ initialValues.email } onChange={handleChange}/>
-
-        <TextField id="outlined-basic" label="Contraseña" variant="outlined"  
-        type="text"  name="password" value={ initialValues.password } onChange={handleChange}/>
-
-        <TextField id="outlined-basic" select label="Rol" variant="outlined"  
-        type="text"  name="roleId" defaultValue="Seleccione un rol" value={initialValues.roleId} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
-            <MenuItem value="1">Admin</MenuItem>
-            <MenuItem value="2">User</MenuItem>
-        </TextField>
-            <input className="input-back-file" accept=".png, .jpg, .jpeg" type="file" name="profile-img" onChange={encodeImageAsURL} placeholder="imagen de perfil"></input>
-            <button className="form-back-submit-btn" type="submit">{ !id ? "Crear" : "Editar" }</button>
+        <h1 className="title-back" >{!id ? "Crear usuario" : (location.includes("edit") ? "Editar Usuario" : "Eliminar Usuario") }</h1>
+        <form className="form-container form-back"  onSubmit={handleSubmit}>
+            <h3 className="title-field-users">Nombre</h3>
+            <input className="input-field input-back" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
+            <h3 className="title-field-users">Email</h3>
+            <input className="input-field input-back" type="text" name="email" value={initialValues.email} onChange={handleChange}   placeholder="Email"   ></input>
+            <h3 className="title-field-users">Contraseña</h3>
+            <input className="input-field input-back" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Password"></input>
+            <h3 className="title-field-users">Rol</h3>
+            <select className="input-field input-back" value={initialValues.roleId} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
+                <option value="" disabled >Select the role</option>
+                <option value="1">Admin</option>
+                <option value="2">User</option>
+            </select>
+            <h3 className="title-field-users">Seleccione una imagen</h3>
+            <input className="input-field input-back-file" accept=".png, .jpg, .jpeg" type="file" name="profile-img" onChange={encodeImageAsURL} placeholder="imagen de perfil"></input>
+            <button className="form-back-submit-btn" type="submit">{!id ? "Crear" : (location.includes("edit") ? "Editar" : "Eliminar")}</button>
         </form>
 
         <Snackbar
                 open={snack.open}
                 severity={snack.severity}
-                autoHideDuration={4000}
+                autoHideDuration={3000}
                 onClose={onCloseSnack}>
                 <Alert onClose={onCloseSnack} severity={snack.severity} sx={{ width: '100%' }}>
                     {snack.message}
                 </Alert>
-        </Snackbar>
+            </Snackbar>
         </>
     );
 }
