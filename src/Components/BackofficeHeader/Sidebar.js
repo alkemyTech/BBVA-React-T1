f0a67359843835fab9b9a0db766ef4813cb28881
Erika Farias <erika.aldana.farias@gmail.com>
2022-05-19 16:19:42 -0300

    Link to HomePage

--- src/Components/BackofficeHeader/Sidebar.js
+++ src/Components/BackofficeHeader/Sidebar.js
@@ -1,44 +1,49 @@
-import React from 'react';
+import React from "react";
 
 export const SidebarData = [
-    {
-        title: 'Actividades',
-        path: '/backoffice/activities',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Categorias',
-        path: '/backoffice/categories',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Novedades',
-        path: '/backoffice/news',
-        cName: 'nav-text'
-    }, 
-    {
-        title: 'Slides',
-        path: '/backoffice/slides',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Testimonios',
-        path: '/backoffice/testimonials',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Usuarios',
-        path: '/backoffice/users',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Miembros',
-        path: '/backoffice/members',
-        cName: 'nav-text'
-    },
-    {
-        title: 'Proyectos',
-        path: '/backoffice/projects',
-        cName: 'nav-text'
-    }
-]
+  {
+    title: "Inicio",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Actividades",
+    path: "/backoffice/activities",
+    cName: "nav-text",
+  },
+  {
+    title: "Categorias",
+    path: "/backoffice/categories",
+    cName: "nav-text",
+  },
+  {
+    title: "Novedades",
+    path: "/backoffice/news",
+    cName: "nav-text",
+  },
+  {
+    title: "Slides",
+    path: "/backoffice/slides",
+    cName: "nav-text",
+  },
+  {
+    title: "Testimonios",
+    path: "/backoffice/testimonials",
+    cName: "nav-text",
+  },
+  {
+    title: "Usuarios",
+    path: "/backoffice/users",
+    cName: "nav-text",
+  },
+  {
+    title: "Miembros",
+    path: "/backoffice/members",
+    cName: "nav-text",
+  },
+  {
+    title: "Proyectos",
+    path: "/backoffice/projects",
+    cName: "nav-text",
+  },
+];
