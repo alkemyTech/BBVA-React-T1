ce9d07a10203d2808f076f75e35ad34d5c0170bd
Agustin <agus.conde18@gmail.com>
2022-05-19 10:54:24 -0300

    Creacion de contexto

---
+++ src/Components/Global/GlobalSpinener.js
@@ -0,0 +1,20 @@
+
+const GlobalSpinner = () => {
+
+
+
+    
+    return (
+        <>
+            <Snackbar
+                open={snack.open}
+                severity={snack.severity}
+                autoHideDuration={3000}
+                onClose={onCloseSnack}>
+                <Alert onClose={onCloseSnack} severity={snack.severity} sx={{ width: '100%' }}>
+                    {snack.message}
+                </Alert>
+            </Snackbar>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+
+export const AppContext = React.createContext();
+
+const appData = {
+  snackbar :{
+    message : "",
+    open: false,
+    severity: "error",
+  },
+  spinner: {
+    open: false,
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
+    <AppContext.Provider value={appData}>
       <App />
+    </AppContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
